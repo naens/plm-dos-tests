e2284f8b0d87d0e2d17cc56a87a5997b3ccc4755
naens <naens@gmx.com>
2018-08-22 22:03:08 +0200

    sys.asm:update

--- sys.asm
+++ sys.asm
@@ -1,4 +1,4 @@
-global entry, term
+global entry, getargs, term
 
 ;****h* plm-exercises/sys
 ;  NAME
@@ -33,6 +33,25 @@ entry:	mov	ax, data
 	mov	ds, ax
 	mov	ax, 0
 	jmp	ax
+
+;****f* sys/getargs
+;  NAME
+;    getargs -- get command line arguments vector
+;  DESCRIPTION
+;    Allocates the vector of pointers to the command line arguments.
+;  PARAMETERS
+;    pargc - pointer to a word value, for the count of arguments
+;    pargv - pointer to an address value, that will contain the address of
+;            the allocated array of adresses of command line arguments.
+;  RETURN VALUE
+;    Doesn't return a value.
+;****
+
+        segment data class=data
+
+        segment code class=code
+
+getargs:
 
 ;****f* sys/term
 ;  NAME
