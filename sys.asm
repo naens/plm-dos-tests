c81d55fa59abb5fa7dd1c61f986aa9c351d8e52a
naens <naens@gmx.com>
2018-08-23 21:53:34 +0200

    sys: get program name

--- sys.asm
+++ sys.asm
@@ -26,15 +26,15 @@ global entry, getargs, term
 ;****
 
         segment data class=data
-psp	resw	1
+psp		resw	1
 
         segment code class=code
 entry:	
 	push	ds		; push psp value
 	mov	ax, data
 	mov	ds, ax
-	mov	es, ax
-	pop	word [psp]	; store psp
+	pop	ax
+	mov	[psp], ax	; store psp
 	mov	ax, 0
 	jmp	ax
 
@@ -50,12 +50,80 @@ entry:
 ;  RETURN VALUE
 ;    Doesn't return a value.
 ;****
-
         segment data class=data
+argv		resw	10
+teststr		db      'test', 0
+hellostr	db      'HELLO!', 0
+tmpstr		resb	100
 
         segment code class=code
-
 getargs:
+	push	bp
+	mov	bp, sp
+
+	mov	di, tmpstr
+	call	getname
+
+; copy the program name from es:bx to local variable
+.l:
+	je	.m
+	mov	al, [es:bx]
+	mov	[ds:di], al
+	cmp	al, 0
+	je	.m
+	inc	bx
+	inc	di
+	jmp	.l
+.m:
+
+; TODO copy cli arguments into argv while counting arguments
+
+	mov	ax, [psp]
+	mov	es, ax
+	mov	ax, [es:80h]
+	mov	es, ax		; set es:0000 to the cli tail
+
+
+	mov	word [argv], teststr
+	mov	word [argv+2], hellostr
+	mov	word [argv+4], tmpstr
+	mov	word [argv+6], 0
+
+	mov	bx, [bp+6]	; set argc value
+	mov	word [bx], 1
+	mov	bx, [bp+4]	; set pointer to argv
+	mov	word [bx], argv
+
+	pop	bp
+	ret
+
+; return the program name string in es:bx (private)
+getname:
+	mov	ax, [psp]
+	mov	es, ax
+	mov	ax, [es:2ch]
+	mov	es, ax
+	mov	bx, 0
+
+.lp:
+	mov	al, [es:bx]
+	cmp	al, 0
+	je	.e
+
+.strlp:
+	mov	al, [es:bx]
+	cmp	al, 0
+	je	.stre
+	inc	bx
+	jmp	.strlp
+.stre:
+	inc	bx
+	dec	cx
+	jmp	.lp
+.e:
+
+	add	bx, 3
+	ret
 
 ;****f* sys/term
 ;  NAME
