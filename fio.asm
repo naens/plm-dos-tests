ed4cda09ded209a3e77ac38fd7476e254a1677f3
naens <naens@gmx.com>
2018-08-25 21:16:10 +0200

    fio: test and update file functions

--- fio.asm
+++ fio.asm
@@ -1,4 +1,4 @@
-global fopen, fclose, freadchr, freadbuf, fwritechr, fwritebuf, \
+global fopen, fclose, freadchr, freadbuf, feof, fwritechr, fwritebuf, \
     fwritestr, fseek, ftrunc
 
 extern prchr, prstr, prcrlf, prhexbyte, prhexword, prstr, readkey, readln
@@ -21,8 +21,9 @@ bufsz		equ	512	; the size of the buffer and of the block
 	segment	data	class=data
 fbuf		resb bufsz	; buffer for read and write
 fhandle		resw 1		; dos file handle
-ncurblk		resw 1		; number of the current block
+curblk		resw 1		; number of the current block
 bufpos		resw 1		; the position in the buffer
+buflen		resw 1		; if last block, number of bytes remaining
 openmode	resb 1		; 2 bits: open mode: 01:r, 10:w, 11:rw (bits: rw)
 bufmodified	resb 1		; boolean: buffer modified or not
 bufloaded	resb 1		; boolean: buffer loaded or not
@@ -60,25 +61,41 @@ fopen:
 	mov	dx, [bp+8]	; dx=filename
 	mov	al, [bp+6] 	; al=r
 	mov	bl, [bp+4]	; bl=w
-	shl	al, 1		; al=2*r
-	or	al, bl		; al=2*r+w
+	shl	bl, 1		; bl=2*w
+	or	al, bl		; al=2*w+r
 	mov	[openmode], al	; store open mode flags
 	dec	al		; convert to dos mode
 	int	dos
-
-; TODO error handling
 	jc .error
 
 	mov	word [fhandle], ax
-	mov	word [ncurblk], 0
+	mov	word [curblk], 0
 	mov	word [bufpos], 0
 	mov	byte [bufmodified], 0
+
+	; skipping loading first block if file not readable (write-only)
+	test	byte [openmode], 1
+	jz	.nold
+
+	; load first block
+	mov	ah, read
+	mov	bx, [fhandle]
+	mov	cx, bufsz
+	mov	dx, fbuf
+	int	dos
+	jc	.error
+	mov	[buflen], ax
+	mov	byte [bufloaded], 1
+	jmp	.endld
+.nold:
 	mov	byte [bufloaded], 0
+
+.endld:
 
 	mov	ax, fbuf	; return value: pfile
 	jmp	.end
 .error:
-	mov	ax, 0
+	mov	ax, 0ffffh
 
 .end:
 	pop	bp
@@ -109,11 +126,132 @@ fclose:
 	pop	bp
 	ret	2
 
+;****f* fio/freadchr
+;  NAME
+;    freadchr -- read a character from a file
+;  DESCRIPTION
+;    Reads a character from a file that has been opened previously in
+;    read-only or read/write mode.
+;  PARAMETERS
+;    pfile - pointer to file data
+;    pchar - pointer to the character to set
+;  RETURN VALUE
+;    0 on success
+;    -1 on error
+;****
+;  freadchr (pfile, pchar)
+;    if not readable then
+;      return -1
+;    end if
+;    if buflen < bufsz and bufpos = buflen - 1 then
+;      return -1
+;    end if
+;    *pchar = fbuf[bufpos]
+;    inc pos
+;    if bufpos = bufsz then
+;      pos := 0
+;      inc curblk
+;      dos read next block
+;    end if
+;    return 0
+;  end freadchr
 
 freadchr:
+	push	bp
+	mov	bp, sp
+
+;	mov	ax, [bp+6]	; pfile
+
+	; if not readable, return -1
+	mov	al, [openmode]
+	test	al, 1
+	jz	.error
+
+        ; if buflen < bufsz and bufpos = buflen then
+	mov	ax, [buflen]	; buflen in ax
+	cmp	ax, bufsz
+	jge	.noteof
+	mov	bx, [bufpos]	; bufpos in bx
+	cmp	ax, bx
+	je	.error
+.noteof:
+
+	; set *pchar = fbuf[bufpos]
+	mov	si, [bufpos]
+	mov	bx, fbuf
+	mov	al, [bx+si]	; al = fbuf[bufpos]
+	mov	bx, [bp+4]	; bx = pchar
+	mov	[bx], al
+	inc	word [bufpos]
+
+	; if bufpos = bufsz then read next block
+	mov	ax, bufpos
+	cmp	ax, bufsz
+	jne	.done
+
+	; load block
+	mov	ah, read
+	mov	bx, [fhandle]
+	mov	cx, bufsz
+	mov	dx, fbuf
+	int	dos
+	jc	.error
+	mov	[buflen], ax
+	mov	word [bufpos], 0
+	inc	word [curblk]
+.done:
+	mov	ax, 0
+	jmp	.end
+.error:
+	mov	ax, 0ffffh
+.end:
+	pop	bp
+	ret	4
+
+
+; reads a single block, updates variables
+
 freadbuf:
 fwritechr:
 fwritebuf:
+
+;****f* fio/feof
+;  NAME
+;    feof -- check if at the end of file
+;  DESCRIPTION
+;    Check if the current position is at the end of file.  End of file means
+;    that no more characters can be read.  The false value indicates that
+;    there is at least one byte in the file that can be read at the current
+;    position.
+;  PARAMETERS
+;    pfile - pointer to file data
+;  RETURN VALUE
+;    Boolean value telling whether the file position is at the end of file.
+;****
+; The end of file condition means that the length of the current buffer is
+; less than maximum (bufsz) and that the current position in the buffer has
+; is after the last byte: no more bytes can be read.
+
+feof:
+	push	bp
+	mov	bp, sp
+
+        ; if buflen < bufsz and bufpos = buflen then return 1 else return 0
+	mov	ax, [buflen]	; buflen in ax
+	cmp	ax, bufsz
+	jge	.noteof
+	mov	bx, [bufpos]	; bufpos in bx
+	cmp	ax, bx
+	jne	.noteof
+.eof:
+	mov	ax, 1
+	jmp	.end
+.noteof:
+	mov	ax, 0
+.end:
+	pop	bp
+	ret	2
+
 fwritestr:
 fseek:
 ftrunc:
