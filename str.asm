eb6fe9c83a6f46d4469d7da53f8b860a87749593
naens <naens@gmx.com>
2018-09-20 21:29:44 +0200

    str: make a first implementation of the subroutines word2xstr and byte2xstr

--- str.asm
+++ str.asm
@@ -1,4 +1,4 @@
-global strcmp, strlen, xstr2word, dstr2int, int2xstr, int2dstr
+global strcmp, strlen, xstr2word, xstr2byte, dstr2int, word2xstr, byte2xstr, int2dstr
 
 %include "cons.inc"
 
@@ -156,11 +156,142 @@ xstr2word:
 	pop	bp
 	ret	2
 
-; convert decimal string to integer
-dstr2int:
+;****f* str/xstr2byte
+;  NAME
+;    xstr2byte -- convert a string to number and return a byte
+;  DESCRIPTION
+;    Converts a hexadecimal representation of a number to a number and
+;    returns it as a single byte.
+;  PARAMETERS
+;    pstr -- pointer to the string to read
+;  RETURN VALUE
+;    Returns the value represented by the string.
+;****
+xstr2byte:
+	jmp	xstr2word
 
-; convert integer to hexadecimal string
-int2xstr:
+;****f* str/dstr2int
+;  NAME
+;    dstr2int -- convert a decimal string to signed integer
+;  DESCRIPTION
+;    Converts a null terminated string, positive or negative to a 16-bit
+;    signed integer.  If the string is empty 0 is returned.
+;  PARAMETERS
+;    pstr - pointer to string representing the integer that has to be
+;    converted.
+;  RETURN VALUE
+;    Returns a signed integer corresponding to the input string.
+;****
+;  Pseudocode:
+;    res := 0
+;    ptr := pstr
+;    while [ptr] >= '0' and [ptr] <= '9' do
+;      res = res * 10
+;      res += [ptr] - '0'
+dstr2int:
+	push	bp
+	mov	bp, sp
+
+	mov	bx, [bp+4]
+	mov	cx, 10
+	mov	ax, 0
+	mov	dh, 0
+.l:
+	mov	dl, [bx]
+	cmp	dl, '0'
+	jb	.e
+	cmp	dl, '9'
+	ja	.e
+	mul	cx
+	sub	dl, 10
+	add	ax, dx
+	inc	bx
+	jmp	.l
+.e:
+	pop	bp
+	ret
+
+;****f* str/word2xstr
+;  NAME
+;    word2xstr -- convert integer to hexadecimal string
+;  DESCRIPTION
+;    Creates a hexadecimal string representation of an integer.  The string
+;    is null-terminated and should be big enough to contain the resulting
+;    string.  The number is read as unsigned.  The string will be of length
+;    4, with leading zeros if needed.
+;  PARAMETERS
+;    w - the word to convert
+;    pbuf - the address where the string will be stored
+;  RETURN VALUE
+;    There is no return value
+;****
+	segment	data	class=data
+digits		db 	'0123456789abcdef'
+
+	segment code	class=code
+word2xstr:
+	push	bp
+	mov	bp, sp
+	mov	ax, [bp+6]
+	mov	di, [bp+4]
+	mov	si, digits
+	mov	bh, 0
+	mov	dx, ax
+	and	dx, 0f0fh
+	mov	bl, dh
+	mov	cl, [bx+si]
+	mov	[di+3], cl
+	mov	bl, dl
+	mov	cl, [bx+si]
+	mov	[di+1], cl
+	and	ax, 0f0f0h	; playing with ax from here
+	mov	cl, 4
+	shr	ax, cl
+	mov	bl, ah
+	mov	cl, [bx+si]
+	mov	[di+2], cl
+	mov	bl, al
+	mov	cl, [bx+si]
+	mov	[di+0], cl
+	mov	byte [di+4], 0
+	pop	bp
+	ret
+
+;****f* str/byte2xstr
+;  NAME
+;    byte2xstr -- create a hexadecimal string representation of a byte
+;  DESCRIPTION
+;    Writes a null-terminated hexadecimal string representation of the byte
+;    given as parameter.  The string can contain a leading zero in order to
+;    have the length of 2.
+;  PARAMETERS
+;    b - the byte to create the string from
+;    pbuf - buffer where to write the string
+;  RETURN VALUE
+;    Does not return anything
+;****
+byte2xstr:
+	push	bp
+	mov	bp, sp
+	mov	al, [bp+6]
+	mov	di, [bp+4]
+	mov	si, digits
+	mov	bh, 0
+	mov	dl, al
+	and	dl, 0fh
+	mov	bl, dl
+	mov	cl, [bx+si]
+	mov	[di+1], cl
+	and	al, 0f0h
+	mov	cl, 4
+	shr	dx, cl
+	mov	bl, al
+	mov	cl, [bx+si]
+	mov	[di+0], cl
+	mov	byte [di+2], 0
+	pop	bp
+	ret
+
 
 ; convert integer to decimal string
 int2dstr:
