442f87988f401ad810812709e3323456ed1a37da
naens <naens@gmx.com>
2018-10-11 22:59:48 +0200

    asm/hello: change entry point to main

--- asm/hello.asm
+++ asm/hello.asm
@@ -2,16 +2,18 @@
 %include "sys.inc"
 %include "dos.def"
 
+global main
+
 	segment stack stack class=stack
-    resb 100h 
+resb 100h
 
         segment data    class=data
 msg db 'Hello, World!', 0Dh, 0Ah, 0
 
         segment code    class=code
-..start:
-	mov	ax, data
-	mov	ds, ax
+main:
+;	mov	ax, data
+;	mov	ds, ax
 
 	mov	ax, msg
 	push	ax
